9987b2a95e5e69568eb6644bbb2bc91801e26c10
fox <me@iamdavidfox.com>
2023-12-27 16:47:41 -0500

    ripped some code but it works

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        await produce();
+        consume();
+    }
+
+    private static async Task produce()
+    {
+        var config = new ProducerConfig { BootstrapServers = "192.168.0.246:9092" };
+
+        // If serializers are not specified, default serializers from
+        // `Confluent.Kafka.Serializers` will be automatically used where
+        // available. Note: by default strings are encoded as UTF8.
+        using (var p = new ProducerBuilder<Null, string>(config).Build())
+        {
+            try
+            {
+                var dr = await p.ProduceAsync("test-topic", new Message<Null, string> { Value = "test" });
+                Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+            }
+            catch (ProduceException<Null, string> e)
+            {
+                Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+            }
+        }
+    }
+
+    private static void consume()
+    {
+        var conf = new ConsumerConfig
+        {
+            GroupId = "test-consumer-group",
+            BootstrapServers = "192.168.0.246:9092",
+            // Note: The AutoOffsetReset property determines the start offset in the event
+            // there are no offsets saved for the consumer group for the topic/partitions of interest.
+            // Earliest: automatically reset the offset to the earliest offset
+            // Latest: automatically reset the offset to the latest offset
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+        {
+            c.Subscribe("test-topic");
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) => {
+                e.Cancel = true; // prevent the process from terminating.
+                cts.Cancel();
+            };
+
+            try
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var cr = c.Consume(cts.Token);
+                        Console.WriteLine($"Consumed message '{cr.Message.Value}' at: '{cr.TopicPartitionOffset}'.");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Error occured: {e.Error.Reason}");
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // Ctrl+C was pressed.
+            }
+            finally
+            {
+                c.Close();
+            }
+        }
+    }
+}
